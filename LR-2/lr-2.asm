4b36368d1de4378f8b7b513a8aed834adedee6db
Artem Bokun <79195348+relsa228@users.noreply.github.com>
2021-09-19 22:09:29 +0300

    Lab 2 and rem Lab 1

--- LR-2/lr-2.asm
+++ LR-2/lr-2.asm
@@ -2,20 +2,147 @@
 .stack 256
 
 data segment
-a   dw   1
-b   dw   2
-c   dw   3
-d   dw   4
+a   dw   0
+b   dw   0
+c   dw   0
+d   dw   0
 max   dw   0
 result   dw   0
+
+;xuinya
+enterA db 'Enter a: $'
+enterB db 'Enter b: $'
+enterC db 'Enter c: $'
+enterDD db 'Enter d: $'
+outResult db 'Result: $'
+errorMessage db 'Input error! Try again.'
+indent  db '', 0Dh, 0Ah, '$'
+
+parA label byte
+maxlenA db 10
+actlenA db ?
+fldA db 10 dup('$')
+
+parB label byte
+maxlenB db 10
+actlenB db ?
+fldB db 10 dup('$')
+
+parC label byte
+maxlenC db 10
+actlenC db ?
+fldC db 10 dup('$')
+
+parD label byte
+maxlenD db 10
+actlenD db ?
+fldD db 10 dup('$')
+;xuinya
 
 data ends
 
 code segment
 assume cs:code, ds:data
+
+outInt2 proc near
+    aam 
+    add ax,3030h 
+    mov dl,ah 
+    mov dh,al 
+    mov ah,02 
+    int 21h 
+    mov dl,dh 
+    int 21h
+outInt2 endp
+
+makeIntend proc near
+    lea dx, indent
+    mov ah, 09
+    int 21h
+    ret
+makeIntend endp
+
+enterNum proc near
+    mov di, 0           
+    mov cx, [bx]            ;в CX количество введенных символов
+    xor ch, ch
+    mov si, 1               ;в SI множитель 
+    @loopMet:
+    push si                 ;сохраняем SI (множитель) в стеке
+    mov si, cx              ;в SI помещаем номер текущего символа 
+    mov ax, [bx+si]         ;в AX помещаем текущий символ 
+    xor ah, ah
+    pop si                  ;извлекаем множитель (SI)из стека
+    sub ax, 30h             ;получаем из символа (AX) цифру
+    mul si                  ;умножаем цифру (AX)на множитель (SI)
+    add di, ax              ;складываем с результирующим числом
+    mov ax, si              ;помещаем множитель (SI) в AX
+    mov dx, 10
+    mul dx                  ;увеличиваем множитель (AX) в 10 раз
+    mov si, ax              ;перемещаем множитель (AX) назад в SI
+    loop @loopMet                ;переходим к предыдущему символу
+    call makeIntend
+    ret
+enterNum endp
+
 start:
         mov ax, data
         mov ds, ax
+        
+        lea dx, enterA                                  ;вводим а   
+        mov ah, 09
+        int 21h
+        lea dx, parA
+        mov ah, 0Ah
+        int 21h
+        lea bx, parA+1                                  ;в BX адрес второго элемента буфера
+        call enterNum
+        mov a, di
+        jmp @firstTry
+
+        @errorInput:                                       ;вводим d 
+        lea dx, errorMessage
+        mov ah, 09
+        int 21h
+        @firstTry:
+        lea dx, enterB                                  ;вводим b   
+        mov ah, 09
+        int 21h
+        lea dx, parB
+        mov ah, 0Ah
+        int 21h
+        lea bx, parB+1                                  
+        call enterNum
+        mov b, di
+        mov ax, b
+        cmp ax, 0
+        je @errorInput
+        mov ax, a
+        mul ax
+        mov bx, b
+        cmp ax, bx
+        je @errorInput
+
+        lea dx, enterC                                  ;вводим c   
+        mov ah, 09
+        int 21h
+        lea dx, parC
+        mov ah, 0Ah
+        int 21h
+        lea bx, parC+1                                  
+        call enterNum
+        mov c, di
+
+        lea dx, enterDD   
+        mov ah, 09                                 
+        int 21h
+        lea dx, parD
+        mov ah, 0Ah
+        int 21h
+        lea bx, parD+1  
+        call enterNum
+        mov d, di
+
         mov ax, a
         imul a
         imul a
@@ -104,6 +231,10 @@ start:
 
     @exit:
         mov ax, result
+        lea dx, outResult                                  
+        mov ah, 09
+        int 21h
+        call outInt2
         mov ah, 4ch
         int   21h
 code ends
