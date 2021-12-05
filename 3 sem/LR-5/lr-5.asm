2b833e35d78661ded0d05da7f90ead99bb0e88e7
Artem Bokun <79195348+relsa228@users.noreply.github.com>
2021-12-05 06:08:26 +0300

    LR-5

--- 3 sem/LR-5/lr-5.asm
+++ 3 sem/LR-5/lr-5.asm
@@ -3,17 +3,32 @@
 
 data segment
 
-masF dw 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-masS dw 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+masF dw 10000 dup(0)
+masS dw 10000 dup(0)
 masFSize dw 0
 masSSize dw 0
+masResSize dw 0
+result dw 0
 buffCX dw 0
+buffBX dw 0
+buffDIF dw 0
+buffBXS dw 0
 str dw 0
+strCount dw 0
 col dw 0
+colCount dw 0
 enterCol db 'Enter the number of columns: $'
 enterStr db 'Enter the number of lines: $'
-error db 'Input error!'
+fillMasF db 'Fill first array: $'
+fillMasS db 'Fill second array: $'
+resultStr db 'Result matrix: $'
+symbolStr db 'Symbol $'
+tireStr db ' - $'
+error db 'Input error!$'
+overflow db 'Overflow!$'
 indent  db '', 0Dh, 0Ah, '$'
+space db ':$'
+spaceS db ' $'
 
 parNum label byte
 maxlenNum db 10
@@ -96,7 +111,6 @@ enterNum proc near
     loop @loopMet                                      
     
     @return:
-    call makeIntend
     ret
 
     @Signed:
@@ -121,19 +135,110 @@ enterNum proc near
 enterNum endp
 
 fillArray proc near ;заполнение массива
+    mov buffBX, 0
+    mov buffCX, 0
+    jmp @fillLoop
+@makeCol:
+    mov colCount, 0
+    inc strCount
 @fillLoop:
+    mov buffBX, bx
+    mov buffCX, cx
+    mov ax, colCount
+    cmp ax, col
+    je @makeCol
+    lea dx, symbolStr
+    mov ah, 09
+    int 21h
+    mov ax, colCount
+    call outInt2
+    lea dx, space
+    mov ah, 09
+    int 21h
+    mov ax, strCount
+    call outInt2
+    lea dx, tireStr
+    mov ah, 09
+    int 21h
+
     lea dx, parNum
     mov ah, 0Ah
     int 21h
-    lea bx, parNum+1    
-    mov buffCX, cx                              
+    lea bx, parNum+1                                 
     call enterNum
-    mov cx, buffCX
+    mov bx, buffBX
     mov [bx], di    ;Переслать DI в элемент массива
-    add bx, 2   ;Разместить в BX адрес следующего элемента массива
+    add bx, 2     ;Разместить в BX адрес следующего элемента массива
+    inc colCount
+    call makeIntend
+    mov cx, buffCX
 loop @fillLoop
+
+mov strCount, 0
+mov colCount, 0
 ret
 fillArray endp
+
+taskProc proc near
+mov colCount, 0
+
+lea di, masF
+mov buffDIF, di
+
+lea bx, masS
+mov buffBXS, bx
+
+jmp @taskLoop
+@taskIntend:
+    call makeIntend
+    mov colCount, 0
+    inc strCount
+    jmp @proceed
+@taskLoop:
+    mov bx, buffBXS
+    mov ax, [bx]
+    mov di, buffDIF
+    mov dx, [di]
+    imul dx
+    jo @overflowLoop
+    mov result, ax
+
+    mov ax, colCount
+    cmp ax, col
+    je @taskIntend
+
+    @proceed:
+
+    mov ax, result
+    mov buffCX, cx
+    call outInt2
+    mov cx, buffCX
+    lea dx, spaceS
+    mov ah, 09
+    int 21h
+
+    mov di, buffDIF
+    add di, 2
+    mov buffDIF, di
+
+    mov bx, buffBXS
+    add bx, 2
+    mov buffBXS, bx
+
+    inc colCount
+
+loop @taskLoop
+
+@end:
+ret
+
+@overflowLoop:
+    lea dx, overflow
+    mov ah, 09
+    int 21h
+    jmp @end
+    
+taskProc endp
 
 makeIntend proc near
     lea dx, indent
@@ -142,14 +247,12 @@ makeIntend proc near
     ret
 makeIntend endp
 
-
-
 start:
     mov ax, data
     mov ds, ax
     jmp @firstTryStr
 
-@errorInputStr:     ;ввод размерности первого массива
+@errorInputStr:     ;ввод размерности массива
     lea dx, error
     mov ah, 09
     int 21h
@@ -163,7 +266,8 @@ start:
     int 21h
     lea bx, parStr+1                                  
     call enterNum
-    cmp di, 5
+    call makeIntend
+    cmp di, 9999
     ja @errorInputStr
     cmp di, 0
     jbe @errorInputStr
@@ -184,70 +288,44 @@ start:
     int 21h
     lea bx, parCol+1                                  
     call enterNum
-    cmp di, 5
-    ja @errorInputCol
+    call makeIntend
     cmp di, 0
     jbe @errorInputCol
     mov col, di
-
     mov ax, str
-    mul col 
-    mov masFSize, ax
-    call makeIntend
-    jmp @firstTryStrS
-
-
-
-
-@errorInputStrS:     ;ввод размерности второго массива
-    lea dx, error
-    mov ah, 09
-    int 21h
-    call makeIntend
-@firstTryStrS:
-    lea dx, enterStr
-    mov ah, 09
-    int 21h
-    lea dx, parStr
-    mov ah, 0Ah
-    int 21h
-    lea bx, parStr+1                                  
-    call enterNum
-    cmp di, 5
-    ja @errorInputStrS
-    cmp di, 0
-    jbe @errorInputStrS
-    mov str, di
-    jmp @firstTryColS
-
-@errorInputColS:
-    lea dx, error
-    mov ah, 09
-    int 21h
-    call makeIntend
-@firstTryColS:
-    lea dx, enterCol
-    mov ah, 09
-    int 21h
-    lea dx, parCol
-    mov ah, 0Ah
-    int 21h
-    lea bx, parCol+1                                  
-    call enterNum
-    cmp di, 5
-    ja @errorInputColS
-    cmp di, 0
-    jbe @errorInputColS
-    mov col, di
-
-    mov ax, str
-    mul col 
+    imul col 
+    jc @errorInputCol
+    cmp ax, 9999
+    ja @errorInputCol
     mov masSSize, ax
-
-
-lea bx, masF    ;Поместить в BX адрес начала массива mas.
+    mov masFSize, ax
+    mov masResSize, ax
+    
+call makeIntend ;Заполнение masF
+lea dx, fillMasF
+mov ah, 09
+int 21h
+lea bx, masF    ;Поместить в BX адрес начала массива masF.
 mov cx, masFSize
+call makeIntend
 call fillArray
+
+call makeIntend ;Заполнение masS
+lea dx, fillMasS
+mov ah, 09
+int 21h
+lea bx, masS    ;Поместить в BX адрес начала массива masS.
+mov cx, masSSize
+call makeIntend
+call fillArray
+
+call makeIntend
+lea dx, resultStr
+mov ah, 09
+int 21h
+call makeIntend
+mov cx, masResSize
+call taskProc
 
 mov ah, 4ch
 int 21h
